79423a4c77e81f463226658aec42999bb53f143c
xian <595876601@qq.com>
2018-09-18 18:16:26 +0800

    a

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const router = require('koa-router')()
 const ajvValidator = require('../lib/middlewares/ajv')
 const wxauthCtrl = require('../lib/controllers/wxauth')
 
+router.get('/', async (ctx, next) => {
+  ctx.body = 'a'
+  }
+)
+
 router.get('/code', async (ctx, next) => {
     await wxauthCtrl.getAccessTokenAPI(ctx, next)
   }
